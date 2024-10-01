9c741b0c64224e3f699ecff77a243cd913d4391d
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-01 05:37:56 +0100

    fix: method returns number not string

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,13 +1,8 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-  const employees = Object.entries(employeesList);
-  for (const [dept, employeeList] of employees) {
-    allEmployees[dept] = [...employeeList];
-  }
   const report = {
-    allEmployees,
+    allEmployees: employeesList,
     getNumberOfDepartments(allEmployees) {
-      return `${Object.keys(allEmployees).length}`;
+      return Object.keys(allEmployees).length;
     },
   };
   return report;
