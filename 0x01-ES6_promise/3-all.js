587995f906b1bc1f012e415261b1d415b8613dfb
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-02 21:27:45 +0100

    fix: refactor function to return promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
-  Promise.all([photo, user])
+  return Promise.all([photo, user])
     .then(([photo, user]) => console.log(`${photo.body} ${user.firstName} ${user.lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
