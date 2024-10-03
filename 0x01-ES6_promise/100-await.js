e3ee94d9f163a2161094576fae322525a80e1ee1
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-03 15:52:47 +0100

    feat: solve extra task 0

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = uploadPhoto();
+    const user = createUser();
+    return { photo, user };
+  } catch (e) {
+    return { photo: null, user: null };
+  }
+}
