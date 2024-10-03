76bdeefbcec18079b03324df5e0d5cbd6b036a3f
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-03 23:21:40 +0100

    feat: task 10 solution

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,43 @@
+export default class Car {
+  constructor(brand, motor) {
+    this.brand = brand;
+    this.motor = motor;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(newBrand) {
+    if (typeof newBrand !== 'string') {
+      throw new TypeError('brand must be a string');
+    }
+    this._brand = newBrand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(newMotor) {
+    if (typeof newMotor !== 'string') {
+      throw new TypeError('motor must be a string');
+    }
+    this._motor = newMotor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(newColor) {
+    if (typeof newColor !== 'string') {
+      throw new TypeError('motor must be a string');
+    }
+    this._color = newColor;
+  }
+
+  cloneCar() {
+    return new this.constructor(this.brand, this.motor, this.color);
+  }
+}
