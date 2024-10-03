0a00bc76d2d0424f54ef009a48ab53ecdfac6612
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-03 21:50:52 +0100

    feat: task 5 solution

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+export default class Building {
+  constructor(sqft) {
+    if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = newSqft;
+  }
+}
