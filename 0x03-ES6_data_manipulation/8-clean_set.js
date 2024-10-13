63219befb74f7254784fbb899d419894fde8ff05
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-13 22:07:41 +0100

    fix:lint issues and undefined strings

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
-  return [...set].filter((str) => str.startsWith(startString))
-    .map((str) => str.slice(startString.length))
+  if (startString.length === 0 || !startString) return '';
+  return [...set].filter((str) => (str ? str.startsWith(startString) : ''))
+    .map((str) => (str ? str.slice(startString.length) : ''))
     .join('-');
 }
