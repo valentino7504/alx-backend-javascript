8f9f641ffd6711b119eafefada41455b258f3599
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-13 22:45:15 +0100

    feat: solved extra task

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,6 +3,6 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const exists = weakMap.get(endpoint);
   if (!exists) weakMap.set(endpoint, 1);
-  else if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
+  else if (weakMap.get(endpoint) >= 4) throw new Error('Endpoint load is high');
   else weakMap.set(endpoint, exists + 1);
 }
