b4563436cda7f39828d11d9f0ba2a2ac5f8c4e0a
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-13 20:12:07 +0100

    fix: handle falsies

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const strArr = [];
-  if (startString === '') return '';
+  if (!startString) return '';
   for (const string of set) {
     if (string.startsWith(startString)) strArr.push(string.split(startString)[1]);
   }
