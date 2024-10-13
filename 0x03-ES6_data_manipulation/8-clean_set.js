09ce303807b109f00e1b09d37f46e8710b08f904
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-13 22:10:39 +0100

    feat: task 8 solution

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,7 @@
-export default function cleanSet(set, startString) {
-  if (startString.length === 0 || !startString) return '';
-  return [...set].filter((str) => (str ? str.startsWith(startString) : ''))
-    .map((str) => (str ? str.slice(startString.length) : ''))
+export default function cleanSet(set, string) {
+  if (string === undefined || string.length === 0) return '';
+  return [...set]
+    .filter((str) => (str !== undefined ? str.startsWith(string) : ''))
+    .map((str) => (str !== undefined ? str.slice(string.length) : ''))
     .join('-');
 }
