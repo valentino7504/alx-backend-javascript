a4f4ba5e751b5ff7ef09eac79259615df31500fd
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-13 19:28:19 +0100

    fix: return dataview of buffer

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,5 +3,5 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer, 0, length);
   int8View[position] = value;
-  return buffer;
+  return new DataView(buffer);
 }
