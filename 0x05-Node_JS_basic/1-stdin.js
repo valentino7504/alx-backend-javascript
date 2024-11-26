44b66d4104240999b4ab00b6e49db45c52661602
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-26 07:40:48 +0100

    fix: ensure not child

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,15 @@
 console.log('Welcome to Holberton School, what is your name?');
-if (!process.stdin.isTTY) {
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
   process.on('exit', () => {
-    console.log('This important software is now closing');
+    process.stdout.write('This important software is now closing\n');
   });
 }
-process.stdin.on('data', (data) => {
-  const name = data.toString();
-  process.stdout.write(`Your name is: ${name}`);
-  process.exit();
-});
