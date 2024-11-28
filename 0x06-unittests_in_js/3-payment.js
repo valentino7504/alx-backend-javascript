24afb71a8859dbe7596e78bd660f4cd0af88041d
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-28 15:16:28 +0100

    fix: function print as required

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,6 +2,6 @@ const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is ${total}`);
+  console.log(`The total is: ${total}`);
 }
 module.exports = sendPaymentRequestToApi;
