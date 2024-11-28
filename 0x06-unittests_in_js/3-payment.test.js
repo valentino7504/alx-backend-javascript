09f2aba04a0bae83616b3f1f142f4b4e9d6f37bb
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-28 15:13:49 +0100

    fix: refactor

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -7,7 +7,8 @@ describe('sendPaymentRequestToApi', () => {
   it('should call Utils.calculateNumber with SUM 102.3, 45.8', () => {
     const spy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(102.3, 45.8);
-    expect(spy.calledOnceWith('SUM', 102.3, 45.8)).to.be.true;
+    expect(spy.calledWith('SUM', 102.3, 45.8)).to.be.true;
+    expect(spy.calledOnce).to.be.true;
     spy.restore();
   });
 });
